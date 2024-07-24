9d202d826dfe12761426c97d120b96a65049b00b
Jaxcoder <codenamejason@outlook.com>
2024-07-24 11:31:02 -0400

    deploy base sepolia

---
+++ script/DeploySippp.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+import {SiPPP} from "../src/SiPPP.sol";
+
+contract DeploySippp is Script {
+    address public admin = 0x2C80552A6f2FD1b32d7783E4c5086899da3933b8;
+    address payable public treasury = payable(0x2C80552A6f2FD1b32d7783E4c5086899da3933b8);
+    address public publicAddress = 0x2C80552A6f2FD1b32d7783E4c5086899da3933b8;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        SiPPP sippp = new SiPPP(admin, publicAddress, treasury);
+
+        console.logAddress(address(sippp));
+
+        vm.stopBroadcast();
+    }
+}
+
+// forge script script/DeploySippp.s.sol:DeploySippp --rpc-url $RPC_URL --broadcast --etherscan-api-key $ETHERSCAN_API_KEY --verify
---
+++ test/SiPPP.t.sol
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SiPPP} from "../src/SiPPP.sol";
+
+contract SiPPPTest is Test {
+    SiPPP public sippp;
+
+    address private admin = vm.addr(uint256(keccak256("ADMIN")));
+    address private app = vm.addr(uint256(keccak256("APP")));
+    address private newApp = vm.addr(uint256(keccak256("NEW_APP")));
+    address private badApp = vm.addr(uint256(keccak256("BAD_APP")));
+    address private userWallet = vm.addr(uint256(keccak256("USER_WALLET")));
+
+    uint256 farcasterId = 12345;
+    address primaryAccount = vm.addr(uint256(keccak256("PRIMARY_ACCOUNT")));
+    string name;
+    string email;
+
+    bytes encodedUserData;
+
+    // SiPPP.UserDevice device = SiPPP.UserDevice({
+    //     deviceId: 1,
+    //     user: userWallet,
+    //     make: "Test Make",
+    //     model: "Test Model",
+    //     serialNumber: "1234567890"
+    // });
+
+    // bytes encodedUserDevice = abi.encode(device);
+
+    //     function setUp() public {
+    //         vm.prank(admin);
+    //         sippp = new SiPPPP(admin, app);
+    //         vm.prank(admin);
+    //         sippp.grantAppRole(app);
+
+    //         name = "Test User";
+    //         email = "test@example.com";
+
+    //         encodedUserData = abi.encode(userWallet, farcasterId, primaryAccount, name, email);
+    //     }
+
+    //     function test_grantAppRole() public {
+    //         vm.prank(admin);
+    //         sippp.grantAppRole(newApp);
+
+    //         bool hasRole = sippp.hasRole(sippp.APP_ROLE(), newApp);
+    //         assertTrue(hasRole, "New app should have APP_ROLE");
+    //     }
+
+    //     function test_revokeAppRole() public {
+    //         vm.prank(admin);
+    //         sippp.revokeAppRole(badApp);
+
+    //         bool hasRole = sippp.hasRole(sippp.APP_BANNED(), badApp);
+    //         assertTrue(hasRole, "Bad app should have APP_BANNED");
+    //     }
+
+    //     function test_registerUser() public {
+    //         vm.prank(app);
+    //         sippp.registerUser(encodedUserData);
+
+    //         SiPPPProvenance.UserAccount memory registeredUser = sippp.getUserAccount(userWallet);
+
+    //         assertEq(registeredUser.farcasterId, farcasterId, "Farcaster ID should match");
+    //         assertEq(registeredUser.primaryAccount, primaryAccount, "Primary account should match");
+    //         assertEq(registeredUser.name, name, "Name should match");
+    //         assertEq(registeredUser.email, email, "Email should match");
+    //     }
+
+    //     function test_registerDevice() public {
+    //         vm.prank(app);
+    //         sippp.registerUser(encodedUserData);
+
+    //         vm.prank(app);
+    //         sippp.registerDevice(userWallet, encodedUserDevice);
+
+    //         SiPPPProvenance.UserDevice memory registeredDevice = sippp.getUserDevice(userWallet, device.deviceId);
+
+    //         assertEq(registeredDevice.deviceId, device.deviceId, "Device ID should match");
+    //         assertEq(registeredDevice.user, device.user, "User should match");
+    //         assertEq(registeredDevice.make, device.make, "Make should match");
+    //         assertEq(registeredDevice.model, device.model, "Model should match");
+    //         assertEq(registeredDevice.serialNumber, device.serialNumber, "Serial number should match");
+    //     }
+
+    //     function test_revert_registerDevice_userNotRegistered() public {
+    //         vm.prank(app);
+    //         vm.expectRevert(SiPPPProvenance.UserNotRegistered.selector);
+    //         sippp.registerDevice(userWallet, encodedUserDevice);
+    //     }
+
+    //     function test_registerPhoto() public {
+    //         vm.prank(app);
+    //         sippp.registerUser(encodedUserData);
+
+    //         vm.prank(app);
+    //         sippp.registerDevice(userWallet, encodedUserDevice);
+
+    //         SiPPPProvenance.PhotoProvenance memory photo = SiPPPProvenance.PhotoProvenance({
+    //             deviceId: 1,
+    //             timestamp: block.timestamp,
+    //             location: "Test Location",
+    //             seedPhrase: "Test Seed Phrase",
+    //             seedAlgorithm: "Test Algorithm",
+    //             user: userWallet,
+    //             photoHash: keccak256(abi.encodePacked("Test Photo")),
+    //             algoConfirmed: false
+    //         });
+
+    //         bytes memory encodedPhoto = abi.encode(photo);
+
+    //         vm.prank(app);
+    //         sippp.registerPhoto(userWallet, photo.deviceId, encodedPhoto);
+
+    //         (
+    //             uint256 deviceId,
+    //             uint256 timestamp,
+    //             string memory location,
+    //             string memory seedPhrase,
+    //             string memory seedAlgorithm,
+    //             address user,
+    //             bytes32 photoHash,
+    //             bool algoConfirmed
+    //         ) = sippp.userPhotos(userWallet, photo.photoHash);
+
+    //         assertEq(deviceId, photo.deviceId, "Device ID should match");
+    //         assertEq(timestamp, photo.timestamp, "Timestamp should match");
+    //         assertEq(location, photo.location, "Location should match");
+    //         assertEq(seedPhrase, photo.seedPhrase, "Seed phrase should match");
+    //         assertEq(seedAlgorithm, photo.seedAlgorithm, "Seed algorithm should match");
+    //         assertEq(user, photo.user, "User should match");
+    //         assertEq(photoHash, photo.photoHash, "Photo hash should match");
+    //         assertEq(algoConfirmed, photo.algoConfirmed, "Algo confirmed should match");
+    //     }
+
+    //     function test_revert_registerPhoto_userNotRegistered() public {
+    //         vm.prank(app);
+
+    //         sippp.registerUser(encodedUserData);
+
+    //         SiPPPProvenance.PhotoProvenance memory photo = SiPPPProvenance.PhotoProvenance({
+    //             deviceId: 1,
+    //             timestamp: block.timestamp,
+    //             location: "Test Location",
+    //             seedPhrase: "Test Seed Phrase",
+    //             seedAlgorithm: "Test Algorithm",
+    //             user: userWallet,
+    //             photoHash: keccak256(abi.encodePacked("Test Photo")),
+    //             algoConfirmed: false
+    //         });
+
+    //         bytes memory encodedPhoto = abi.encode(photo);
+
+    //         vm.prank(app);
+    //         vm.expectRevert(SiPPPProvenance.DeviceNotRegistered.selector);
+    //         sippp.registerPhoto(userWallet, photo.deviceId, encodedPhoto);
+
+    //         (
+    //             uint256 deviceId,
+    //             uint256 timestamp,
+    //             string memory location,
+    //             string memory seedPhrase,
+    //             string memory seedAlgorithm,
+    //             address user,
+    //             bytes32 photoHash,
+    //             bool algoConfirmed
+    //         ) = sippp.userPhotos(userWallet, photo.photoHash);
+    //     }
+}
