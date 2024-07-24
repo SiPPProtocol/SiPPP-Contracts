60cd15f548f71a7fd8e971a78045b06ac693230d
Jaxcoder <codenamejason@outlook.com>
2024-07-24 09:48:57 -0400

    update and merge conflicts

--- src/SiPPP.sol
+++ src/SiPPP.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.21;
-// pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-// import "hardhat/console.sol";
 // Open Zeppelin
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -165,6 +162,7 @@ contract SiPPP is AccessControl, RecoverMessage {
     event PhotoVerified(string _photoHash, address requester);
     event Verified(bool verified);
     event PublicAddy(address publicAddy);
+    event TreasuryUpdated(address _treasury);
 
     struct TransactionData {
         uint256 timestamp;
@@ -175,11 +173,14 @@ contract SiPPP is AccessControl, RecoverMessage {
         string photoIpfsHash;
     }
 
+    uint256 private REV_SHARE_PCNT = 90;
+
     bytes32 public constant APP_ROLE = keccak256("APP_ROLE");
     bytes32 public constant APP_BANNED = keccak256("APP_BANNED");
 
     address private s_appAddress;
     address private immutable s_admin;
+    address payable private TREASURY;
 
     string[] private s_photoIds;
 
@@ -200,8 +201,14 @@ contract SiPPP is AccessControl, RecoverMessage {
         _;
     }
 
-    constructor(address _admin, address _publicAddy) {
+    modifier positiveMsgValue() {
+        if (msg.value <= 0) revert("Please pay to sippp with Ether.");
+        _;
+    }
+
+    constructor(address _admin, address _publicAddy, address payable _treasury) {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        TREASURY = _treasury;
         s_appAddress = _publicAddy; // "0x2C80552A6f2FD1b32d7783E4c5086899da3933b8";
         s_admin = _admin;
     }
@@ -239,34 +246,60 @@ contract SiPPP is AccessControl, RecoverMessage {
 
     function updatePubAddy(address _publicAddy) public onlyAdmin {
         s_appAddress = _publicAddy;
+
         emit PublicAddy(_publicAddy);
     }
 
+    function updateTreasury(address payable _treasury) public onlyAdmin {
+        TREASURY = _treasury;
+
+        emit TreasuryUpdated(_treasury);
+    }
+
+    function updateRevShareCut(uint256 _revSharePcnt) public onlyAdmin {
+        REV_SHARE_PCNT = _revSharePcnt;
+    }
+
     /// @notice Registers a new photo
-    /// @param _wallet The address of the wallet
+    /// @param _npm_wallet The address of the wallet
     /// @param _sipppTxn The photo to register
-    function registerPhoto(address _wallet, TransactionData calldata _sipppTxn)
+    function registerPhoto(address payable _npm_wallet, TransactionData calldata _sipppTxn)
         public
         payable
         onlyApp(_sipppTxn.photoIpfsHash, _sipppTxn.rawSig)
+        positiveMsgValue
     {
         // (TransactionData memory transaction) = abi.decode(_sipppTxn, (TransactionData));
 
+        if (_npm_wallet == address(0)) {
+            (bool success,) = TREASURY.call{value: msg.value}("");
+            require(success, "Transfer failed");
+        } else {
+            uint256 _sippp_cut = msg.value * REV_SHARE_PCNT / 100;
+            uint256 _3rd_party_cut = msg.value - _sippp_cut;
+            (bool success1,) = TREASURY.call{value: _sippp_cut}("");
+            require(success1, "Transfer failed");
+            (bool success2,) = _npm_wallet.call{value: _3rd_party_cut}("");
+            // if (!success2) revert("Transfer failed");
+            require(success2, "Transfer failed");
+        }
+
         s_photoIds.push(_sipppTxn.photoIpfsHash);
-        s_userAddresses.push(_wallet);
-        s_userPhotos[_wallet].push(_sipppTxn);
+        s_userAddresses.push(msg.sender);
+        s_userPhotos[msg.sender].push(_sipppTxn);
         s_photoSippped[_sipppTxn.photoIpfsHash] = true;
-        s_userSippped[_wallet] = true;
+        s_userSippped[msg.sender] = true;
 
         emit PhotoRegistered(_sipppTxn.photoIpfsHash, _sipppTxn.timestamp);
     }
 
     function verifyPhotoProvenance(
-        address _wallet, // why wallet? should people need the sippper's wallet to verify?
+        address, // why wallet? should people need the sippper's wallet to verify?
         string calldata _photoIpfsHash
     ) public returns (bool) {
         if (s_photoSippped[_photoIpfsHash]) {
             emit PhotoVerified(_photoIpfsHash, msg.sender);
+
             return true;
         }
         return false;
